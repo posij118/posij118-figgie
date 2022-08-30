80f47aa9b91a8987ec7a69e0d493c3652d09e410
posij118 <schuttka@seznam.cz>
2022-08-30 16:28:19 +0800

    Add server-side redirections

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 router.get("*", (req, res) => {
   let url = path.join(__dirname, "../view/build", "index.html");
+    console.log(url);
   if (!url.startsWith("/app/"))
     // since we're on local windows
     url = url.substring(1);
